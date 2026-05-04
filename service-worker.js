d7b679818235a56adad5a021e2cab4a0b603ffda
sijomonps <sijomon700@gmail.com>
2026-05-04 17:19:50 +0530

    first commit

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'flappy-aljo-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/game.js',
+  '/render.js',
+  '/utils.js',
+  '/particles.js',
+  '/assets/images/icon.png',
+  '/assets/images/splash.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).catch(() => {
+          // Fallback if offline and not in cache
+          return caches.match('/index.html');
+        });
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
